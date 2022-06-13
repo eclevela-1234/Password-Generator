3230a7580b6a348ab996c6e7e29f9f47ef3a3b24
eliotCleveland <eliothead@live.com>
2022-06-12 22:45:36 -0400

    prompts for criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,86 @@
 // Assignment code here
+var passCriteria = {};
 
+var alertInvalid = function () {
+  window.alert("Please enter a valid response.");
+};
+
+// prompt user for password criteria and pass into passCriteria object
+var promptCriteria = function () {
+  var promptLength = function () {
+    // prompt user for password length
+    passCriteria.passLen = window.prompt(
+      "Please enter the length of password (8-128 characters)"
+    );
+    if (passCriteria.passLen < 8 || passCriteria.passLen > 128) {
+      alertInvalid();
+      promptLength();
+    }
+  };
+
+  var promptBool = function () {
+    // confirm user for case sensitive
+    var caseResponse = window.confirm(
+      "Is this password case-sensitive?\n\n[Ok] for YES, [Cancel] for NO"
+    );
+    if (caseResponse) {
+      passCriteria.caseSense = true;
+    } else {
+      passCriteria.caseSense = false;
+    }
+    // };
+    // var promptNumbers = function () {
+    // confirm user for case sensitive
+    var numResponse = window.confirm(
+      "Will the password contain numbers?\n\n[Ok] for YES, [Cancel] for NO"
+    );
+    if (numResponse) {
+      passCriteria.numb = true;
+    } else {
+      passCriteria.numb = false;
+    }
+    // };
+    // var promptSpecial = function () {
+    // confirm user for case sensitive
+    var specResponse = window.confirm(
+      "Will the password contain special characters?\n\n[Ok] for YES, [Cancel] for NO"
+    );
+    if (specResponse) {
+      passCriteria.spec = true;
+    } else {
+      passCriteria.spec = false;
+    }
+  };
+  var confirmCrit = function () {
+    var confirm = window.confirm(
+      "Please confirm these criteria for your password:\n\n[Ok] for YES, [Cancel] for NO\n\nPassword Length: " +
+        passCriteria.passLen +
+        "\nCase-sensitive: " +
+        passCriteria.caseSense +
+        "\nNumerics: " +
+        passCriteria.numb +
+        "\nSpecial Characters: " +
+        passCriteria.spec
+    );
+    if (!confirm) {
+      window.alert("No problem! Let's try again!");
+      promptCriteria();
+    }
+  };
+
+  promptLength();
+  promptBool();
+  confirmCrit();
+  // promptCase();
+  // promptNumbers();
+  // promptSpecial();
+  console.log(passCriteria);
+};
+
+var generatePassword = function () {
+  promptCriteria();
+  return "password";
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,6 +92,7 @@ function writePassword() {
 
   passwordText.value = password;
 
+  console.log("password");
 }
 
 // Add event listener to generate button

09e0ec1126167a4aabd7fcf1b3f7627a93270ae5
eliotCleveland <eliothead@live.com>
2022-06-13 00:02:27 -0400

    added passChar string

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,8 @@ var promptCriteria = function () {
       promptLength();
     }
   };
-
+  // prompt user for boolean responses
   var promptBool = function () {
-    // confirm user for case sensitive
     var caseResponse = window.confirm(
       "Is this password case-sensitive?\n\n[Ok] for YES, [Cancel] for NO"
     );
@@ -28,9 +27,7 @@ var promptCriteria = function () {
     } else {
       passCriteria.caseSense = false;
     }
-    // };
-    // var promptNumbers = function () {
-    // confirm user for case sensitive
+
     var numResponse = window.confirm(
       "Will the password contain numbers?\n\n[Ok] for YES, [Cancel] for NO"
     );
@@ -39,9 +36,7 @@ var promptCriteria = function () {
     } else {
       passCriteria.numb = false;
     }
-    // };
-    // var promptSpecial = function () {
-    // confirm user for case sensitive
+
     var specResponse = window.confirm(
       "Will the password contain special characters?\n\n[Ok] for YES, [Cancel] for NO"
     );
@@ -67,17 +62,30 @@ var promptCriteria = function () {
       promptCriteria();
     }
   };
-
   promptLength();
   promptBool();
   confirmCrit();
-  // promptCase();
-  // promptNumbers();
-  // promptSpecial();
-  console.log(passCriteria);
+};
+
+
+// randomizer function testing below
+var passChar = {
+  specials: [' ', '!' ,'"', '#', '$', '%', '&', "'", '(' , ")", "*", "+", ',', "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', ']', '^', '_', "`", "{", "|", "}", "~"],
+  lowers: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  uppers: ['A','B','C,','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+  Numers: ['0','1','2','3','4','5','6','7','8','9']
+};
+
+var randomizer = function (y) {
+  len = passChar[y].length; 
+
+  // len = testes.x.length;
+  // console.log(testes.x, len);
+console.log(len);
 };
 
 var generatePassword = function () {
+  randomizer("lowers");
   promptCriteria();
   return "password";
 };
